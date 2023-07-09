9e0c7ba283ce21d7f503bd738e99d7a495943682
asma laptop <asmaalashwali@hotmail.com>
2023-07-09 22:22:34 +0300

    import navbar & footer

---
+++ JavaScript/footer.js
@@ -0,0 +1,6 @@
+export const footer = `
+<p>
+© 2023 Coffee Beans. All Rights Reserved.
+<a href="Privacy Policy.html">Privacy Policy</a>
+</p>`;
+document.getElementById("footer").innerHTML = footer;
